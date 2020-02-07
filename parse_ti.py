388db4ac6f3db0f5e38000a36459f4a1d9a3869f
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2020-02-06 21:56:11 -0500

    add ti

---
+++ parse_ti.py
@@ -0,0 +1,32 @@
+import csv
+import pandas as pd
+
+# Prep data
+temp_data = 'raw_data/ti/temperature.csv'
+
+
+def calc_temp_avgs():
+    min_voltage_mean = calc_avgs_from_csv(
+        temp_data, "Supply voltage  (Min) (V)")
+    max_voltage_mean = calc_avgs_from_csv(
+        temp_data, "Supply voltage (Max) (V)")
+    max_current_mean = calc_avgs_from_csv(
+        temp_data, "Supply current(Max) (uA)")
+
+    print(min_voltage_mean)
+
+
+def calc_avgs_from_csv(data_file, col_name):
+    data = pd.read_csv(data_file)
+    raw_col = pd.to_numeric(
+        data[col_name],
+        errors='coerce'
+    )
+    return raw_col.mean()
+
+
+def main():
+    calc_temp_avgs()
+
+
+main()
